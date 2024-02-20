450fd85e982679964cea56369d89e5093f69b5b1
Adob720 <160674259+Adob720@users.noreply.github.com>
2024-02-20 23:44:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openArticles() {
+    window.location.href = 'site-ul meu.html';
+}
+
+function openContact() {
+    window.location.href = 'contact.html';
+}
+
+function openReviews() {
+    window.location.href = 'reviews.html';
+}
